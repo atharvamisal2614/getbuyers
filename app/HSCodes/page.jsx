72e5a60e965bf3ae98fe186e08d901d3fc63e27b
atharva <misalatharva407@gmail.com>
2025-02-01 17:43:32 +0530

    udpdate HSCode

--- app/HSCodes/page.jsx
+++ app/HSCodes/page.jsx
@@ -22,21 +22,21 @@ const HSCodes = () => {
       title: 'Trade Trend Analysis',
       description: 'Monitor fluctuations in import and export volumes over time to spot emerging trends and new opportunities. Analyzing trade patterns helps businesses adapt to market demands, optimize strategies, and stay competitive in a rapidly evolving global economy.',
     },
-    // {
-    //   icon: <RiShieldCheckLine size={40} className="text-blue-600" />,
-    //   title: 'Business Planning',
-    //   description: 'Utilize market data to make informed decisions on product development, pricing, and marketing strategies. Analyzing trends, consumer demand, and competitor insights helps business optimize offerings, maximize profitability, and stay ahead in the market.',
-    // },
-    // {
-    //   icon: <FaTruckMoving size={40} className="text-blue-600" />,
-    //   title: 'Avoiding Dangers',
-    //   description: 'Understand import/export procedures across countries to identify potential trade regulation and customs compliance issues. Analyzing policies helps businesses avoid delays, reduce risks, and ensure smooth international transactions while maintaining legal compliance.',
-    // },
-    // {
-    //   icon: <BsGraphUpArrow size={40} className="text-blue-600" />,
-    //   title: 'Logistic Optimization',
-    //   description: 'Analyze trade data on ports and transportation to plan efficient shipping routes and logistics. Optimizing supply chains through data-driven insights reduces costs, improves delivery times, and enhances overall operationnal efficiency in global trade.',
-    // },
+    {
+      icon: <RiShieldCheckLine size={40} className="text-blue-600" />,
+      title: 'Business Planning',
+      description: 'Utilize market data to make informed decisions on product development, pricing, and marketing strategies. Analyzing trends, consumer demand, and competitor insights helps business optimize offerings, maximize profitability, and stay ahead in the market.',
+    },
+    {
+      icon: <FaTruckMoving size={40} className="text-blue-600" />,
+      title: 'Avoiding Dangers',
+      description: 'Understand import/export procedures across countries to identify potential trade regulation and customs compliance issues. Analyzing policies helps businesses avoid delays, reduce risks, and ensure smooth international transactions while maintaining legal compliance.',
+    },
+    {
+      icon: <BsGraphUpArrow size={40} className="text-blue-600" />,
+      title: 'Logistic Optimization',
+      description: 'Analyze trade data on ports and transportation to plan efficient shipping routes and logistics. Optimizing supply chains through data-driven insights reduces costs, improves delivery times, and enhances overall operationnal efficiency in global trade.',
+    },
   ];
 
   return (
@@ -62,7 +62,7 @@ const HSCodes = () => {
         </div>
       </div>
 
-      {/* <MultiLevelAccordion /> */}
+      <MultiLevelAccordion />
     </section>
   );
 };
